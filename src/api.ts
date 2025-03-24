cd1200e909b40e52406e9487ec6284844758b3cb
Ari Gunnar Kristjónsson <agk19@hi.is>
2025-03-24 10:49:24 +0000

    link fix?

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import { Category, Paginated, Question } from './types';
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:8000";; 
+const BASE_URL = "https://vef2-2025-v3-synilausn-8u79.onrender.com/"; // process.env.NEXT_PUBLIC_BASE_URL ?? 
 
 export class QuestionsApi {
   async fetchFromApi<T>(url: string): Promise<T | null> {
