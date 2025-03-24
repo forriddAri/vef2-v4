8f4f8922292a057b99197eb0a6d29e8a6ff76f54
Ari Gunnar Kristjónsson <agk19@hi.is>
2025-03-24 12:34:48 +0000

    virkar ettta nuna?

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import { Category, Paginated, Question } from './types';
 
-const BASE_URL = "https://vef2-2025-v3-synilausn-8u79.onrender.com/"; // process.env.NEXT_PUBLIC_BASE_URL ?? 
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8000";
 
 export class QuestionsApi {
   async fetchFromApi<T>(url: string): Promise<T | null> {
